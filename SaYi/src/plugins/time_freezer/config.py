30fdf8522df6d288bc086c165b40730def89c097
DZYCD <126797731+DZYCD@users.noreply.github.com>
2024-10-15 23:00:04 +0800

    Delete SaYi/src/plugins directory

--- SaYi/src/plugins/time_freezer/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import Field, BaseModel
-
-
-class Config(BaseModel):
-    apscheduler_autostart: bool = True
-    apscheduler_log_level: int = 30
-    apscheduler_config: dict = Field(
-        default_factory=lambda: {"apscheduler.timezone": "Asia/Shanghai"}
-    )
